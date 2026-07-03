6e64328347afe12fd7eeb059f6386f8a40739f47
AlisaNyaXD <55809431+AlisaNya@users.noreply.github.com>
2026-07-04 02:17:54 +0500

    Initial commit

---
+++ Source/UnrealEssenceTemplateCore/Private/UnrealEssenceTemplateCoreBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealEssenceTemplateCoreBPLibrary.h"
+#include "UnrealEssenceTemplateCore.h"
+
+UUnrealEssenceTemplateCoreBPLibrary::UUnrealEssenceTemplateCoreBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UUnrealEssenceTemplateCoreBPLibrary::UnrealEssenceTemplateCoreSampleFunction(float Param)
+{
+	return -1;
+}
+
