dad5db13ac7c72df901ebcdb9db8b228a441fbea
AlisaNyaXD <55809431+AlisaNya@users.noreply.github.com>
2026-08-11 10:37:11 +0500

    Create UnrealEssenceTemplateCoreLogChannels.h

---
+++ Source/UnrealEssenceTemplateCore/Public/UnrealEssenceTemplateCoreLogChannels.h
@@ -0,0 +1,7 @@
+﻿// Copyright UnrealEssence. All Rights Reserved.
+
+#pragma once
+
+#include "Logging/LogMacros.h"
+
+UNREALESSENCETEMPLATECORE_API DECLARE_LOG_CATEGORY_EXTERN(LogUnrealEssenceTemplateCoreRuntime, Log, All);
